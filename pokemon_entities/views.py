dbfbad547bbe0e5b7efdc31944a3ef6e60ee6121
Flash-kir <flash-kir@yandex.ru>
2022-12-02 16:58:56 +0300

    show pokemons on mainpage map.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,11 +1,11 @@
 import folium
 import json
 import os
+from urllib.parse import urlparse
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from .models import Pokemon, PokemonEntity
-from pogomap import settings
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -18,7 +18,7 @@ DEFAULT_IMAGE_URL = (
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
     icon = folium.features.CustomIcon(
-        image_url,
+        'http://127.0.0.1:8000' + image_url,
         icon_size=(50, 50),
     )
     folium.Marker(
@@ -30,16 +30,17 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+#    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+#        pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
+    for pokemon in Pokemon.objects.all():
+        for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon):
             add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+                folium_map, 
+                pokemon_entity.lat,
+                pokemon_entity.lon,
+                pokemon.image.url
             )
 
     pokemons_on_page = []
