2da685f4127b7c965e6d481c21288e8e2231d9ba
Flash-kir <flash-kir@yandex.ru>
2022-12-02 15:16:00 +0300

    show pokemons on mainpage.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,11 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='media', blank=True, null=True)
 
     def __str__(self):
         return self.title
+
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,11 @@
 import folium
 import json
+import os
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import Pokemon, PokemonEntity
+from pogomap import settings
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -40,11 +43,11 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.image,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
