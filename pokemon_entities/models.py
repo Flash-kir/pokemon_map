7b2febb09ae92580700d9ca94fda46771670016a
Flash-kir <flash-kir@yandex.ru>
2022-12-05 12:25:47 +0300

    add field names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,19 +6,22 @@ class Pokemon(models.Model):
         max_length=200,
         blank=True,
         null=True,
-        default=None
+        default=None,
+        verbose_name='Имя на русском',
         )
     title_en = models.CharField(
         max_length=200,
         blank=True,
         null=True,
-        default=None
+        default=None,
+        verbose_name='Имя на английском',
         )
     title_jp = models.CharField(
         max_length=200,
         blank=True,
         null=True,
-        default=None
+        default=None,
+        verbose_name='Имя на японском',
         )
     next_evolution = models.ForeignKey(
         'Pokemon',
@@ -26,7 +29,8 @@ class Pokemon(models.Model):
         null=True,
         blank=True,
         default=None,
-        related_name='evolution_next'
+        related_name='evolution_next',
+        verbose_name='Эволюционирует в',
         )
     previous_evolution = models.ForeignKey(
         'Pokemon',
@@ -34,23 +38,72 @@ class Pokemon(models.Model):
         null=True,
         blank=True,
         default=None,
-        related_name='evolution_previous'
+        related_name='evolution_previous',
+        verbose_name='Эволюционирует из',
         )
-    description = models.TextField(null=True, blank=True, default=None)
-    image = models.ImageField(upload_to='media', blank=True, null=True)
+    description = models.TextField(
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Описание',
+        )
+    image = models.ImageField(
+        upload_to='media',
+        blank=True,
+        null=True,
+        verbose_name='Изображение',
+        )
 
     def __str__(self):
         return self.title_ru
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.SET_NULL, null=True)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(null=True, default=None)
-    disappeared_at = models.DateTimeField(null=True, default=None)
-    level = models.FloatField(blank=True, null=True, default=None)
-    health = models.FloatField(blank=True, null=True, default=None)
-    strength = models.FloatField(blank=True, null=True, default=None)
-    defence = models.FloatField(blank=True, null=True, default=None)
-    stamina = models.FloatField(blank=True, null=True, default=None)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Покемон',
+        )
+    lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
+    appeared_at = models.DateTimeField(
+        null=True,
+        default=None,
+        verbose_name='Время появления',
+        )
+    disappeared_at = models.DateTimeField(
+        null=True,
+        default=None,
+        verbose_name='Время исчезновения',
+        )
+    level = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Уровень'
+        )
+    health = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Здоровье'
+        )
+    strength = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Сила'
+        )
+    defence = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Защита'
+        )
+    stamina = models.FloatField(
+        blank=True,
+        null=True,
+        default=None,
+        verbose_name='Выносливость'
+        )
